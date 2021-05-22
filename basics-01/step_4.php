b7d7179b26f0ee8837369c345556a9ff2e9d13f9
omar <omarlb512@gmail.com>
2021-05-22 11:45:27 +0300

    step 4

--- basics-01/step_4.php
+++ basics-01/step_4.php
@@ -1,5 +1,5 @@
 <?php
-$name="omar";
+$name="omar abdallah ";
 $position="full stack developer ";
 $github_url="github.com/omarabdallah96";
 echo "hello I am ".$nam. "i am a ".$position."check my github link ".$github_url;
