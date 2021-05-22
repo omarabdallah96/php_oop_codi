15ad1ca59fe3066a3fe6cca95a6991111244d493
omar <omarlb512@gmail.com>
2021-05-22 12:05:28 +0300

    step 7

---
+++ basics-01/step_7.php
@@ -0,0 +1,15 @@
+<?php
+$birth_year=1996;
+$current_year=2021;
+if($current_year-$birth_year>18){
+echo
+"You can drive";
+}
+
+else{
+    echo
+    "You still a kid, go and play GTA";    
+
+}
+
+?>
