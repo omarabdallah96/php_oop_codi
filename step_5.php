34329b42da9e9d5fb5764e2c39d0ee2afa5b39c4
omar <omarlb512@gmail.com>
2021-05-22 11:39:36 +0300

    step 5

--- step_5.php
+++ step_5.php
@@ -1,12 +1,25 @@
 <?php
+
 $course_name = "php  course";
 $enrolled_students=30;
 $price=10.5;
-$on_discount=true;
+$on_discount=false;
 
 echo "Course $course_name: Rust, the language of no bugs";
+echo"<br>";
+
 echo "Enrolled Students: $enrolled_students";
 echo"<br>";
 echo "Course price: $price $ USD";
+echo"<br>";
 
-echo "Course on discount: $on_discount "?>
+if($on_discount==true){
+
+    echo"Course on discount:YES";
+
+}
+else{
+    echo"Course on discount:NO";
+}
+
+?>
