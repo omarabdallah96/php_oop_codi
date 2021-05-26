27e766472178f03abd4266aa8cce81a50d74b950
omar <omarlb512@gmail.com>
2021-05-26 12:39:03 +0300

    step 8

--- Exercise 01/auth.php
+++ Exercise 01/auth.php
@@ -1,11 +1,8 @@
 <?php
 
-// فتح الجلسه لاستدعاء قيمتها
 session_start();
 
-// داله للتحقق من وجود قيمه فى جلسة username
 if(!isset($_SESSION['user_nam'])){
-	// اذا كانت القيمه فارغه يتم الانتقال الى صفحة تسجيل الدخول
 	header("Location: home.php");
 	exit();
 }
--- basics-03/step_8.php
+++ basics-03/step_8.php
@@ -1,20 +1,51 @@
 <?php
 $transactions = array(
-    array(
-        "id" => 1,
+    "music" => array(
+        "id" => 5,
         "name"=> "Gaby",
         "email"=> "gaby@codi.tech"
     ),
-    array(
+    "mp3" =>  array(
        "id" => 3,
        "name"=> "omar",
        "email"=> "omar@codi.tech"
     )
 );
 foreach ( $transactions as $key => $value ){
+   
 
-    echo "$key= $value\n";
+   $id=$transactions[$key]["id"] ;
+    
+    $name=$transactions[$key]["name"] ;
 
+    $email=$transactions[$key]["email"] ;
+
+
+    echo 
+    "<div class='grid-item' >
+    <br>
+    <br>
+    <br>
+
+    <table style='width: 100%; text-align: center;'  border = '1'>
+        <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+        </tr>
+
+        <tr>
+            <td>$id</td>
+            <td>$name$</td>
+            <td>$email</td>
+
+        </tr>
+
+    </table> 
+    </div>
+    </div>
+
+    ";
 
 }
 
